985834b5aa99e2111bde2d8c7f3c21e8d7f9f515
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2016-05-19 21:25:11 +1200

    test stuff

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    first_name "MyString"
-    last_name "MyString"
-    name "MyString"
-    username "MyString"
+	first_name Faker::Name.first_name 
+	last_name Faker::Name.last_name
+    name "#{first_name} #{last_name}"
+    username first_name
+    password 'password'
+	confirmed_at Date.today
+
   end
 end
