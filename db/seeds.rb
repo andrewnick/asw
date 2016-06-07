fe7c0d4c5cdbfefdadf21969d8c7aacca34ac52e
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2016-06-07 23:48:40 +1200

    add all seeds back

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,9 @@ password = 'aswedding'
 
 family = Family.create(family_name: last_name)
 
-# User.create(first_name: "Ann", last_name: last_name, family: family, name: "Ann Nicholas", password: password)
-# User.create(first_name: "Steve", last_name: last_name, family: family, name: "Steve Nicholas", password: password)
-# User.create(first_name: "Andrew", last_name: last_name, family: family, name: "Andrew Nicholas", password: password)
-# User.create(first_name: "Matt", last_name: last_name, family: family, name: "Matt Nicholas", password: password)
-# User.create(first_name: "Jeff", last_name: last_name, family: family, name: "Jeff Nicholas", password: password)
+User.create(first_name: "Ann", last_name: last_name, family: family, name: "Ann Nicholas", password: password)
+User.create(first_name: "Steve", last_name: last_name, family: family, name: "Steve Nicholas", password: password)
+User.create(first_name: "Andrew", last_name: last_name, family: family, name: "Andrew Nicholas", password: password)
+User.create(first_name: "Matt", last_name: last_name, family: family, name: "Matt Nicholas", password: password)
+User.create(first_name: "Jeff", last_name: last_name, family: family, name: "Jeff Nicholas", password: password)
 User.create(first_name: "Sarah", last_name: last_name, family: family, name: "Sarah Nicholas", password: password)
